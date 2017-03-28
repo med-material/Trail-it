b947aba0b7cf04488ff5b4acdde1437164fd1596
Bastian Ilsø <bastianilso@gnome.org>
2017-03-28 17:53:56 +0200

    blurImageScaler: scale the blurry background image slowly.

---
+++ Assets/blurImageScaler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blurImageScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale += new Vector3 (0.00005f, 0.00005f, 0.00005f);
+	}
+}
