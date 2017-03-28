a436d8c66a37782faa86f28c0da28b8adccb4d55
Bastian Ilsø <bastianilso@gnome.org>
2017-03-28 17:46:44 +0200

    moveBlurLines: moves the blurry grey lines in the main menu

---
+++ Assets/moveBlurLines.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBlurLines : MonoBehaviour {
+
+	private Vector3 originalPos;
+
+
+	// Use this for initialization
+	void Start () {
+		originalPos = this.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3 (0.0f, 0.2f, 0.0f);
+		if (this.transform.position.y > 600) {
+			this.transform.position = originalPos;
+		}
+	}
+}
