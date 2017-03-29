38a8f3f745d63f73146ee59499544e117099135c
Bastian Ilsø <bastianilso@gnome.org>
2017-03-29 10:03:37 +0200

    moveBlurLines: use a relative value for position reset

--- Assets/moveBlurLines.cs
+++ Assets/moveBlurLines.cs
@@ -16,7 +16,7 @@ public class moveBlurLines : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.transform.position += new Vector3 (0.0f, 0.2f, 0.0f);
-		if (this.transform.position.y > 600) {
+		if (this.transform.position.y > originalPos.y + 600) {
 			this.transform.position = originalPos;
 		}
 	}
