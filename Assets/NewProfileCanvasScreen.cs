603b41356f1bc2cca32d7c98fde94d407812410b
Bastian Ilsø <bastianilso@gnome.org>
2018-04-02 17:30:33 +0200

    NewProfileCanvasScreen: Make e-mail field visible when editing it
    This is a simple attempt at making the e-mail field visible while
    it is being edited.

--- Assets/NewProfileCanvasScreen.cs
+++ Assets/NewProfileCanvasScreen.cs
@@ -30,6 +30,28 @@ public class NewProfileCanvasScreen : MonoBehaviour {
 	[SerializeField]
 	private Toggle shouldProtectSettings;
 
+	[SerializeField]
+	private RectTransform rectPanel;
+	private float defaultAnchoredPositionY;
+
+	public void Start()
+	{
+		defaultAnchoredPositionY = rectPanel.anchoredPosition.y;
+	}
+
+	public void Update()
+	{
+		if (TouchScreenKeyboard.visible && emailField.isFocused == true)
+		{
+			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY + 250.0f);
+		}
+		else
+		{
+			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY);
+		}
+	}
+
+
 	public void SetNameAndCreateProfile() {
 		Debug.Log ("shouldProtect: " + shouldProtectSettings.isOn);
 		profileManager.AddNewProfile (nameField.text, emailField.text, shouldUpload.isOn, shouldProtectSettings.isOn);
@@ -37,4 +59,5 @@ public class NewProfileCanvasScreen : MonoBehaviour {
 		nameField.text = "";
 		emailField.text = "";
 	}
+
 }
