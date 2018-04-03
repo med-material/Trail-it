05aa4145a438b8bdfdc1c8a14f283eaf0fe439b4
Bastian Ilsø <bastianilso@gnome.org>
2018-04-03 22:09:28 +0200

    BackgroundImage: Rotate between background images
    Just some nice eye candy - we change the background
    randomly between each training session.

---
+++ Assets/BackgroundRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundRotation : MonoBehaviour {
+
+	[SerializeField]
+	private Sprite[] backgrounds;
+
+	[SerializeField]
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeBackground()
+	{
+		int r = Random.Range(0, backgrounds.Length);
+		spriteRenderer.sprite = backgrounds[r];
+	}
+}
