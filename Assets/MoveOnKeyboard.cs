1f195aefbcfd5a66ac704719ee231f48cb80e0ac
Bastian Ilsø <bastianilso@gnome.org>
2018-04-04 13:41:44 +0200

    MoveOnKeyboard: Split into seperate script
    We previously introduced a neat little update to
    NewProfileScreen which would detect whether an input field was
    focused and move the canvas upwards. Now we need this for the
    first time user experience too, so let's split it into a
    separate script.

---
+++ Assets/MoveOnKeyboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveOnKeyboard : MonoBehaviour {
+
+	[SerializeField]
+	private InputField inputField;
+
+	[SerializeField]
+	private float moveDistance = 250.0f;
+
+	[SerializeField]
+	private RectTransform rectPanel;
+	private float defaultAnchoredPositionY;
+
+	// Use this for initialization
+	public void Start()
+	{
+		defaultAnchoredPositionY = rectPanel.anchoredPosition.y;
+	}
+	
+	// Update is called once per frame
+	public void Update()
+	{
+		if (TouchScreenKeyboard.visible && inputField.isFocused == true)
+		{
+			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY + moveDistance);
+		}
+		else
+		{
+			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY);
+		}
+	}
+}
--- Assets/NewProfileCanvasScreen.cs
+++ Assets/NewProfileCanvasScreen.cs
@@ -30,28 +30,6 @@ public class NewProfileCanvasScreen : MonoBehaviour {
 	[SerializeField]
 	private Toggle shouldProtectSettings;
 
-	[SerializeField]
-	private RectTransform rectPanel;
-	private float defaultAnchoredPositionY;
-
-	public void Start()
-	{
-		defaultAnchoredPositionY = rectPanel.anchoredPosition.y;
-	}
-
-	public void Update()
-	{
-		if (TouchScreenKeyboard.visible && emailField.isFocused == true)
-		{
-			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY + 250.0f);
-		}
-		else
-		{
-			rectPanel.anchoredPosition = new Vector2(rectPanel.anchoredPosition.x, defaultAnchoredPositionY);
-		}
-	}
-
-
 	public void SetNameAndCreateProfile() {
 		Debug.Log ("shouldProtect: " + shouldProtectSettings.isOn);
 		profileManager.AddNewProfile (nameField.text, emailField.text, shouldUpload.isOn, shouldProtectSettings.isOn);
