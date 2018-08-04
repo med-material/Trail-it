7accd775caa139573ee8886ebdb5c272ef9464d1
Bastian Ilsø <bastianilso@gnome.org>
2018-08-04 16:56:04 +0200

    PrivacyCanvas: Add data collection information to conform to GDPR
    The data collection is completely optional but still important
    to explain in the user interface. This commit adds explanatory
    text and an ability to sign off from data collection after
    the fact.

---
+++ Assets/PrivacyCanvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyCanvas : MonoBehaviour {
+
+    [SerializeField]
+    private ProfileManager profileManager;
+
+    [SerializeField]
+    private GameObject signOffButton;
+
+    [SerializeField]
+    private GameObject signedOffText;
+
+	// Use this for initialization
+	void Start () {
+        if (profileManager.GetUploadPolicy()) {
+            signOffButton.SetActive(true);
+            signedOffText.SetActive(false);
+        } else {
+            signOffButton.SetActive(false);
+            signedOffText.SetActive(true);            
+        }
+            
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
